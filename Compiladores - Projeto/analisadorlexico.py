bdcc7a00b9a4d321dd9ddbfafb2c90cd29636ca5
Henrique <henriquerojinhas@gmail.com>
2023-10-06 08:02:52 -0300

    Erros corrigidos!! Projeto ok para apresentação!!

--- Compiladores - Projeto/analisadorlexico.py
+++ Compiladores - Projeto/analisadorlexico.py
@@ -1,29 +1,29 @@
 import re
 
-# Definir as expressões regulares para cada tipo de token
 patterns = [
-    (r'\d+', 'NUM_INT'),                  # Números Inteiros
-    (r'\d+\.\d+', 'NUM_DEC'),             # Números Decimais
-    (r'[a-zA-Z_]\w*', 'ID'),             # Identificadores
-    (r'"[^"]*"', 'TEXTO'),               # Constantes de Texto
-    (r'int|float|char|boolean|void|if|else|for|while|scanf|println|main|return', 'PALAVRA_RESERVADA'),  # Palavras Reservadas
-    (r'\/\/[^\n]*', 'COMENTARIO'), # Comentários em uma linha
-    (r'\/\*[\s\S]*?\*\/', 'COMENTARIO_MULTI_LINHA'), # Comentários em múltiplas linhas
-    (r'\+|-|\*|\/|%|&&|\|\||!|>|>=|<|<=|!=|==|=|\(|\)|\[|\]|\{|\}|.|;', 'OPERADOR'),  # Operadores e Símbolos Especiais
-    (r'\s+', None)                       # Espaços em branco (ignorados)
+    (r'\d+', 'NUM_INT'),
+    (r'\d+\.\d+', 'NUM_DEC'),
+    (r'[a-zA-Z_]\w*', 'ID'),
+    (r'"[^"]*"', 'TEXTO'),
+    (r'int|float|char|boolean|void|if|else|for|while|scanf|println|main|return', 'PALAVRA_RESERVADA'),
+    (r'\/\/[^\n]*', 'COMENTARIO_UMA_LINHA'),
+    (r'\/\*[\s\S]*?\*\/', 'COMENTARIO_MULTI_LINHA'),
+    (r'\+|-|\*|\/|%|&&|\|\||!|>|>=|<|<=|!=|==|=|\(|\)|\[|\]|\{|\}|.|;', 'OPERADOR'),
+    (r'\s+', None)
 ]
 
-# Função para tokenizar o código-fonte
 def tokenize(code):
     tokens = []
     while code:
         for pattern, token_type in patterns:
             match = re.match(pattern, code, re.DOTALL)
             if match:
-                if token_type:
-                    if token_type != 'COMENTARIO_MULTI_LINHA':  # Ignorar comentários de várias linhas
-                        tokens.append((match.group(0), token_type))
-                code = code[len(match.group(0)):].lstrip()
+                lexeme = match.group(0)
+                if token_type == 'ID' and lexeme == 'length':
+                    raise SyntaxError('"length" não é permitido.')
+                if token_type != 'COMENTARIO_MULTI_LINHA':
+                    tokens.append((lexeme, token_type))
+                code = code[len(lexeme):].lstrip()
                 break
         else:
             raise SyntaxError(f'Token inválido: {code}')
@@ -36,7 +36,7 @@ def ler_codigo_de_arquivo(nome_arquivo):
     return codigo
 
 # Ler código-fonte de um arquivo chamado "input.txt"
-nome_arquivo = 'Compiladores - Projeto\input.txt'
+nome_arquivo = 'input.txt'
 try:
     codigo_fonte = ler_codigo_de_arquivo(nome_arquivo)
     tokens = tokenize(codigo_fonte)
